34a97b957bca4ef907780fc05a5851f7fc91aa1f
HungryBunny-wei <243249439@qq.com>
2019-10-16 14:38:53 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,20 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import {Overlay, OverlayKeyboardDispatcher, OverlayRef} from '@angular/cdk/overlay';
 
 @Component({
   selector: 'nz-demo-alert-basic',
   template: `
       <div #ref style="display: none;">
-        <div class="ant-modal-mask"></div>
-        <div class="ant-modal-wrap">
-          <div class="ant-modal">
+        <div class="ant-modal-mask" (click)="changeModal(false)"></div>
+        <div class="ant-modal-wrap" style="pointer-events: none;">
+        <!-- 内容区 -->
+          <div class="ant-modal" style="pointer-events: auto;">
             <div class=ant-modal-raduis></div>
             <img src="../assets/detail.png">
           </div>
         </div>
       </div>
-    <button nz-button (click)="open()" nzType="primary">打开</button>
+    <button nz-button (click)="changeModal(true)" nzType="primary">打开</button>
   `,
   styles: [
     `
@@ -24,26 +25,34 @@ import {Overlay, OverlayKeyboardDispatcher, OverlayRef} from '@angular/cdk/overl
     `
   ]
 })
-export class NzDemoAlertBasicComponent {
+export class NzDemoAlertBasicComponent implements AfterViewInit {
 @ViewChild('ref', {static: false}) ref: ElementRef<HTMLElement>;
 
   private container: OverlayRef;
-
   constructor(
     private overlay: Overlay,
     private overlayKeyboardDispatcher: OverlayKeyboardDispatcher,
     ){
 
   }
-
-    ngOnInit(): void {
+  ngAfterViewInit(){
     // Place the modal dom to elsewhere
     this.container = this.overlay.create();
-  }
-
-  open(){
-    console.log(this.ref.nativeElement);
-    this.ref.nativeElement.style.display = 'block'
     this.container.overlayElement.appendChild(this.ref.nativeElement);
   }
+
+  changeModal(visible: boolean){
+    if (visible) {
+      if (this.container instanceof OverlayRef) {
+        this.ref.nativeElement.style.display = 'block';
+        this.overlayKeyboardDispatcher.add(this.container);
+      }
+    } else {
+      if (this.container instanceof OverlayRef) {
+        this.ref.nativeElement.style.display = 'none';
+        this.overlayKeyboardDispatcher.remove(this.container);
+      }
+    }
+  }
+
 }
